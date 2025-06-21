e8e2875d53a22497e270eff23d0af73432964557
Demonofpower <emil.peter21@gmail.com>
2025-06-21 11:42:53 +0200

    Added highscore list placeholder

---
+++ PolytopiaB2.Carrier/Hubs/HighscoreHub.cs
@@ -0,0 +1,26 @@
+﻿using PolytopiaBackendBase;
+using PolytopiaBackendBase.Auth;
+using PolytopiaBackendBase.Game;
+
+namespace PolytopiaB2.Carrier.Hubs;
+
+public partial class PolytopiaHub
+{
+    public async Task<ServerResponseList<HighscoreViewModel>> GetHighscores(
+        TribeHighscoresBindingModel model)
+    {
+        var highscores = new List<HighscoreViewModel>();
+
+        var highscore = new HighscoreViewModel();
+        highscore.PolytopiaUserId = Guid.Empty;
+        highscore.Username = "Not implemented yet";
+        highscore.Score = 1;
+        highscore.TribeType = 1;
+        highscore.AvatarStateData = Convert.FromHexString("70000000280000000C000000000000000D00000000000000180000000000000023000000000000002900000000000000");
+        highscore.GameVersions = new List<ClientGameVersionViewModel>();
+
+        highscores.Add(highscore);
+
+        return new ServerResponseList<HighscoreViewModel>(highscores);
+    }
+}
