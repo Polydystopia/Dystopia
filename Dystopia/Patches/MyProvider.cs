5aa40bca92b82f0fe9333e97cc39d53de68f3dce
joukepouke <73423163+joukepouke@users.noreply.github.com>
2025-07-01 17:49:51 +0200

    Fix paths (#6)

--- Dystopia/Patches/MyProvider.cs
+++ Dystopia/Patches/MyProvider.cs
@@ -4,13 +4,13 @@ public class MyProvider : IPolytopiaDataProvider
 {
     public string LoadAvatarData(int version)
     {
-        var avatarDataPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "External", "Data", "AvatarData", "1.txt");
+        var avatarDataPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", "AvatarData", "1.txt");
         return File.ReadAllText(avatarDataPath);
     }
 
     public string LoadGameLogicData(int version)
     {
-        var gameLogicDataPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "External", "Data", "GameLogicData", "19.txt");
+        var gameLogicDataPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", "GameLogicData", "19.txt");
         return File.ReadAllText(gameLogicDataPath);
     }
 }
