188ad96264333c44b15f74eeb699bbe31cc41e1e
joukepouke <73423163+joukepouke@users.noreply.github.com>
2025-07-04 13:02:01 +0200

    Ensure all cache is saved to disk on shutdown (#20)
    * Ensure all cache is saved to disk on shutdown
    
    * Actually save to disk instead of mocking

--- Dystopia/Services/Cache/CacheService.cs
+++ Dystopia/Services/Cache/CacheService.cs
@@ -42,7 +42,7 @@ public class CacheService<T> : ICacheService<T>
 
     public void CleanStaleCache(TimeSpan staleTime, PolydystopiaDbContext dbContext)
     {
-        _logger.LogDebug("Cleaning stale cache.");
+        _logger.LogDebug($"CacheService<{typeof(T)}>: Cleaning stale cache.");
         foreach (var (id, (v, lastUsed, saveToDisk)) in _cache)
         {
             if (DateTime.Now - lastUsed > staleTime)
@@ -50,6 +50,15 @@ public class CacheService<T> : ICacheService<T>
                 TryRemove(id);
                 saveToDisk(dbContext);
             }
+        }
+    }
+
+    public void SaveAllCacheToDisk(PolydystopiaDbContext dbContext)
+    {
+        _logger.LogDebug($"CacheService<{typeof(T)}>: Saving all cache to disk");
+        foreach (var (id, (v, lastUsed, saveToDisk)) in _cache)
+        {
+            saveToDisk(dbContext);
         }
     }
 }
