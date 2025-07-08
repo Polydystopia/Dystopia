4d401516227409c94ba3e8206fcf41a48df7e5f3
Demonofpower <9366819+Demonofpower@users.noreply.github.com>
2025-07-08 10:23:42 +0200

    Other login types (#23)
    * moved auto/login http routes to own controller
    
    * added empty remaining login endpoints
    
    * Added dev certificate
    
    * implemented temporary google play login by deviceId
    
    * Remove IP address logging
    
    ---------
    
    Co-authored-by: Jouke van Dam <jouke@tayami.net>

---
+++ Dystopia/Controllers/AuthController.cs
@@ -0,0 +1,231 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Dystopia.Database.User;
+using Dystopia.Services.Steam;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using PolytopiaBackendBase;
+using PolytopiaBackendBase.Auth;
+using PolytopiaBackendBase.Notifications;
+
+namespace Dystopia.Controllers;
+
+[ApiController]
+[Route("api/auth")]
+public class AuthController : ControllerBase
+{
+    private readonly IPolydystopiaUserRepository _userRepository;
+
+    private readonly ISteamService _steamService;
+
+    private readonly ILogger<AuthController> _logger;
+
+    public AuthController(IPolydystopiaUserRepository userRepository, ISteamService steamService, ILogger<AuthController> logger)
+    {
+        _userRepository = userRepository;
+        _steamService = steamService;
+        _logger = logger;
+    }
+
+    private PolytopiaToken CreateToken(PolytopiaUserViewModel userFromDb)
+    {
+        var token = new PolytopiaToken();
+
+        var claims = new List<Claim>
+        {
+            new("nameid", userFromDb.PolytopiaId.ToString()),
+            new("unique_name", userFromDb.GetUniqueNameInternal()),
+            new("AspNet.Identity.SecurityStamp", "PCSD6HQ3RTGJDIWAT4BBJY3IFW5ARY3J"), //TODO: what is this?
+            new("steam", userFromDb.SteamId)
+        };
+
+        var key = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes("higul0u9pgwojaingwagvupöjoahg8wag890zuahgvbuaagau9j")); //TODO: key
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var now = DateTime.UtcNow;
+        var expiryDate = now.AddDays(1);
+
+        var jwtToken = new JwtSecurityToken(
+            issuer: "Issuer",
+            audience: "Audience",
+            claims: claims,
+            notBefore: now,
+            expires: expiryDate,
+            signingCredentials: creds
+        );
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        token.JwtToken = tokenHandler.WriteToken(jwtToken);
+        token.ExpiresAt = expiryDate;
+
+        token.User = userFromDb;
+
+        return token;
+    }
+
+    [Route("login_steam")]
+    public async Task<ActionResult<ServerResponse<PolytopiaToken>>> LoginSteam([FromBody] SteamLoginBindingModel? model)
+    {
+        if (model?.SteamAuthTicket?.Data == null)
+        {
+            _logger.LogInformation(
+                "Steam login attempt failed. Invalid auth ticket data");
+            return BadRequest("Invalid auth ticket data");
+        }
+
+        var parsedSteamTicket = _steamService.ParseTicket(model.SteamAuthTicket.Data, model.DeviceId);
+        if (parsedSteamTicket == null)
+        {
+            _logger.LogInformation(
+                "Steam login attempt failed. Invalid auth ticket data");
+            return Forbid("Invalid auth ticket data");
+        }
+
+        var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"); //TODO: Hack use DI later
+        var isDevEnv = string.Equals(env, Environments.Development, StringComparison.OrdinalIgnoreCase);
+
+        var username = !isDevEnv ? await _steamService.GetSteamUsernameAsync(parsedSteamTicket.SteamID) : model.DeviceId;
+        if (string.IsNullOrEmpty(username))
+        {
+            _logger.LogWarning("Could not get steam username for steamId {steamId}", parsedSteamTicket.SteamID);;
+            return BadRequest("Username parse error.");
+        }
+
+        var userFromDb = await _userRepository.GetBySteamIdAsync(parsedSteamTicket.SteamID, username);
+
+        var token = CreateToken(userFromDb);
+
+        var json = JsonConvert.SerializeObject(new ServerResponse<PolytopiaToken>(token));
+
+        return Content(json, "application/json");
+    }
+
+    [Route("login_google_play")]
+    public async Task<ActionResult<ServerResponse<PolytopiaToken>>> LoginGooglePlay(
+        LoginGooglePlayBindingModel model)
+    {
+        //TODO we need to find a way to properly handle model.AuthCode. Maybe it will not be possible to use the original one and we have to patch the game app. For now we will use the deviceId as username.
+        var fakeGooglePlaySteamAppTicket = _steamService.ParseTicket(new []{Byte.MaxValue, }, model.DeviceId);
+
+        var userFromDb = await _userRepository.GetBySteamIdAsync(fakeGooglePlaySteamAppTicket.SteamID, model.DeviceId);
+
+        var token = CreateToken(userFromDb);
+
+        var json = JsonConvert.SerializeObject(new ServerResponse<PolytopiaToken>(token));
+
+        return Content(json, "application/json");
+    }
+
+    [Route("login_nintendo_service_account")]
+    public async Task<ActionResult<ServerResponse<PolytopiaToken>>> LoginNintendoServiceAccount(
+        LoginNintendoServiceAccountBindingModel model)
+    {
+        var body = new ServerResponse<PolytopiaToken>(
+            errorCode: ErrorCode.LoginFailed,
+            errorMessage: "This login method is not implemented yet. Please use another.",
+            innerMessage: null
+        );
+
+        return StatusCode(
+            StatusCodes.Status501NotImplemented,
+            body
+        );
+    }
+
+    [Route("login_tesla")]
+    public async Task<ActionResult<ServerResponse<PolytopiaToken>>> LoginTesla(LoginTeslaBindingModel model)
+    {
+        var body = new ServerResponse<PolytopiaToken>(
+            errorCode: ErrorCode.LoginFailed,
+            errorMessage: "This login method is not implemented yet. Please use another.",
+            innerMessage: null
+        );
+
+        return StatusCode(
+            StatusCodes.Status501NotImplemented,
+            body
+        );
+    }
+
+    [Route("login_ios")]
+    public async Task<ActionResult<ServerResponse<PolytopiaToken>>> LoginIos(LoginGameCenterBindingModel model)
+    {
+        var body = new ServerResponse<PolytopiaToken>(
+            errorCode: ErrorCode.LoginFailed,
+            errorMessage: "This login method is not implemented yet. Please use another.",
+            innerMessage: null
+        );
+
+        return StatusCode(
+            StatusCodes.Status501NotImplemented,
+            body
+        );
+    }
+
+    [Route("login_ios_v2")]
+    public async Task<ActionResult<ServerResponse<PolytopiaToken>>> LoginIosV2(LoginGameCenterV2BindingModel model)
+    {
+        var body = new ServerResponse<PolytopiaToken>(
+            errorCode: ErrorCode.LoginFailed,
+            errorMessage: "This login method is disabled. Please use another.",
+            innerMessage: null
+        );
+
+        return StatusCode(
+            StatusCodes.Status405MethodNotAllowed,
+            body
+        );
+    }
+
+    [Route("login_fake")]
+    public async Task<ActionResult<ServerResponse<PolytopiaToken>>> LoginFake(LoginFakeBindingModel model)
+    {
+        var body = new ServerResponse<PolytopiaToken>(
+            errorCode: ErrorCode.LoginFailed,
+            errorMessage: "This login method is disabled. Please use another.",
+            innerMessage: null
+        );
+
+        return StatusCode(
+            StatusCodes.Status405MethodNotAllowed,
+            body
+        );
+    }
+
+    [Route("login_debug_legacy")]
+    public async Task<ActionResult<ServerResponse<PolytopiaToken>>> LoginDebugLegacyUser(LoginFakeBindingModel model)
+    {
+        var body = new ServerResponse<PolytopiaToken>(
+            errorCode: ErrorCode.LoginFailed,
+            errorMessage: "This login method is disabled. Please use another.",
+            innerMessage: null
+        );
+
+        return StatusCode(
+            StatusCodes.Status405MethodNotAllowed,
+            body
+        );
+    }
+
+    [Route("steam_notifications")]
+    public ServerResponse<SteamNotificationsModels.RequestSteamNotificationsResponse> ActivateSteamNotifications(
+        [FromBody] SteamNotificationsModels.RequestSteamNotificationsBindingModel model) //TODO
+    {
+        return new ServerResponse<SteamNotificationsModels.RequestSteamNotificationsResponse>(
+            new SteamNotificationsModels.RequestSteamNotificationsResponse()
+            {
+                Was_Created = false,
+                Allow_Notifications = true
+            });
+    }
+
+    [Route("whoami")]
+    public async Task<ServerResponse<PolytopiaToken>> WhoAmI() //TODO
+    {
+        return new ServerResponse<PolytopiaToken>(new PolytopiaToken());
+    }
+}
