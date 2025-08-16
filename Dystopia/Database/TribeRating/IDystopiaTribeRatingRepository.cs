f8250c7da90de03bcc9370e4916724ddfd37c1f9
Demonofpower <emil.peter21@gmail.com>
2025-08-16 13:03:34 +0200

    Added tribe rating support

---
+++ Dystopia/Database/TribeRating/IDystopiaTribeRatingRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Dystopia.Database.TribeRating;
+
+public interface IDystopiaTribeRatingRepository
+{
+    Task<IList<TribeRatingEntity>?> GetByUserAsync(Guid userId);
+    Task<bool> AddOrUpdateAsync(TribeRatingEntity entity);
+}
